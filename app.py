86114d317ea0472a96dc3953739aa41a0bcb405d
nathan sheryak <nathanraysheryak@gmail.com>
2022-05-06 10:38:35 -0400

    remove print + lint with black

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Hello_World(Resource):
-  def get(self, message):
-    print("WHAT??")
-    return { 'message': message }
 
-api.add_resource(Hello_World, '/hello/<string:message>')
+class Hello_World(Resource):
+    def get(self, message):
+        return {"message": message}
+
+
+api.add_resource(Hello_World, "/hello/<string:message>")
 
 app.run(port=5000)
