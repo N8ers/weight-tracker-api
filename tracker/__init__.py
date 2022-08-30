e47ac84166a1461d4a70b60c97ad432b9d39988f
sheryak_n <nathanraysheryak@gmail.com>
2022-08-30 06:33:12 -0400

    make dummy crud user endpoints

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -9,4 +9,27 @@ def create_app():
     def hi():
         return "<h1>Hi there!</h1>"
 
+    """
+    User Routes (CRUD)
+    """
+    @app.route("/users")
+    def create_user():
+        pass
+
+    @app.route("/users")
+    def get_all_users():
+        pass
+
+    @app.route("/users/<id:int>")
+    def get_user_by_id():
+        pass
+
+    @app.route("/users")
+    def update_user_by_id():
+        pass
+
+    @app.route("/users/<id:int>")
+    def delete_user():
+        pass
+
     return app
