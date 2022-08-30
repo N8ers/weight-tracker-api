201908d7163dcf2b58f63b2cac7ed56748cb51b3
sheryak_n <nathanraysheryak@gmail.com>
2022-08-30 07:33:45 -0400

    create user model

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
 
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    db = SQLAlchemy(app)
+
+    """
+    User Model
+    """
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(80), unique=True, nullable=False)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+
+        def __repr__(self):
+            return '<User %r>' % self.username
 
     @app.route("/")
     def hi():
@@ -20,16 +34,16 @@ def create_app():
     def get_all_users():
         pass
 
-    @app.route("/users/<id:int>")
-    def get_user_by_id():
+    @app.route("/users/<int:id>")
+    def get_user_by_id(id):
         pass
 
     @app.route("/users")
     def update_user_by_id():
         pass
 
-    @app.route("/users/<id:int>")
-    def delete_user():
+    @app.route("/users/<int:id>")
+    def delete_user(id):
         pass
 
     return app
