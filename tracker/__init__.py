7574abf8a075ea152f0f4bfbdb272b7a9e908b17
sheryak_n <nathanraysheryak@gmail.com>
2022-08-31 17:21:18 -0400

    move users routes to blueprint

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -1,12 +1,8 @@
 from flask import Flask
-from flask_apispec import use_kwargs, marshal_with
-from marshmallow import fields
 
 from tracker.extensions import db, migrate, ma
 
-from tracker.blueprints import hi
-
-from tracker.models.users import User, UserSchema, user_schema, users_schema
+from tracker.blueprints import hi, users
 
 
 def create_app():
@@ -18,48 +14,6 @@ def create_app():
     ma.init_app(app)
 
     app.register_blueprint(hi.blueprint)
-
-    """
-    User Routes (CRUD)
-    """
-    @app.route("/users", methods=["POST"])
-    @use_kwargs({"username": fields.Str(), "email": fields.Str()})
-    @marshal_with(UserSchema)
-    def create_user(username, email):
-        new_user = User(username=username, email=email)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return user_schema.dump(new_user), 200
-
-    @app.route("/users", methods=["GET"])
-    def get_all_users():
-        users = User.query.all()
-
-        return users_schema.dump(users), 200
-
-    @app.route("/users/<int:id>", methods=["GET"])
-    def get_user_by_id(id):
-        user = User.query.get(id)
-
-        return user_schema.dump(user), 200
-
-    @app.route("/users", methods=["PUT"])
-    @use_kwargs({"id": fields.Int(), "username": fields.Str(), "email": fields.Str()})
-    @marshal_with(UserSchema)
-    def update_user_by_id(id, username, email):
-        user = User.query.get(id)
-        user.username = username
-        user.email = email
-        db.session.commit()
-
-        return user_schema.dump(user), 200
-
-    @app.route("/users/<int:id>", methods=["DELETE"])
-    def delete_user(id):
-        User.query.filter(User.id == id).delete()
-        db.session.commit()
-
-        return f"user {id} has been deleted.", 200
+    app.register_blueprint(users.blueprint)
 
     return app
---
+++ tracker/blueprints/users.py
@@ -0,0 +1,54 @@
+from flask import Blueprint
+from flask_apispec import use_kwargs, marshal_with
+from marshmallow import fields
+
+from tracker.extensions import db
+
+from tracker.models.users import UserSchema, User, user_schema, users_schema
+
+blueprint = Blueprint("users", __name__, url_prefix="/")
+
+
+@blueprint.route("/users", methods=["POST"])
+@use_kwargs({"username": fields.Str(), "email": fields.Str()})
+@marshal_with(UserSchema)
+def create_user(username, email):
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.dump(new_user), 200
+
+
+@blueprint.route("/users", methods=["GET"])
+def get_all_users():
+    users = User.query.all()
+
+    return users_schema.dump(users), 200
+
+
+@blueprint.route("/users/<int:id>", methods=["GET"])
+def get_user_by_id(id):
+    user = User.query.get(id)
+
+    return user_schema.dump(user), 200
+
+
+@blueprint.route("/users", methods=["PUT"])
+@use_kwargs({"id": fields.Int(), "username": fields.Str(), "email": fields.Str()})
+@marshal_with(UserSchema)
+def update_user_by_id(id, username, email):
+    user = User.query.get(id)
+    user.username = username
+    user.email = email
+    db.session.commit()
+
+    return user_schema.dump(user), 200
+
+
+@blueprint.route("/users/<int:id>", methods=["DELETE"])
+def delete_user(id):
+    User.query.filter(User.id == id).delete()
+    db.session.commit()
+
+    return f"user {id} has been deleted.", 200
