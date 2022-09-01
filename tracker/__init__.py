5a433bedabf13b4437df5b88cdf0f00d4171230e
sheryak_n <nathanraysheryak@gmail.com>
2022-09-01 06:23:16 -0400

    move blueprint regist

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -2,18 +2,20 @@ from flask import Flask
 
 from tracker.extensions import db, migrate, ma
 
-from tracker.blueprints import hi, users
-
 
 def create_app():
-
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
     db.init_app(app)
     migrate.init_app(app, db)
     ma.init_app(app)
+    register_blueprints(app)
+
+    return app
+
+
+def register_blueprints(app):
+    from tracker.blueprints import hi, users
 
     app.register_blueprint(hi.blueprint)
     app.register_blueprint(users.blueprint)
-
-    return app
